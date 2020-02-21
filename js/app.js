32cd414e726960bdab8c54f5321b9506b0039abc
Alexey Babushkin <007-99@mail.ru>
2020-02-22 01:07:53 +0300

    Домашнее задание 20 урок

--- js/app.js
+++ js/app.js
@@ -92,7 +92,7 @@ window.addEventListener('DOMContentLoaded', function(){
         document.addEventListener('click',(event)=>{
 
             let target = event.target;
-            console.log(target);
+            // console.log(target);
             // console.log('target: ', menu.classList.contains('active-menu'));
            
             if (target.closest('.menu')) {
@@ -104,13 +104,13 @@ window.addEventListener('DOMContentLoaded', function(){
 
             }
             if (!target.closest('.active-menu')) {
-                console.log('target', target);
+                // console.log('target', target);
 
                 menu.classList.remove ('active-menu');
             }
          
-            if (target.tagName.toUpperCase() ==='A'&& !target.closest('.close-btn')) {
-                slowScroll(event);
+            if (target.tagName.toUpperCase() ==='A'&& !target.closest('.close-btn') && !target.closest('.portfolio-btn')) {
+                slowScroll(event);portfolio-btn
             } 
            
 
@@ -288,11 +288,144 @@ window.addEventListener('DOMContentLoaded', function(){
 
     };
     tabs();
-
+    
+    // слайдер
     const slider = () =>{
+        const slide = document.querySelectorAll('.portfolio-item'),
+              btn = document.querySelectorAll('.portfolio-btn'),
+              parrentDot = document.querySelector('.portfolio-dots'),
+              slider = document.querySelector('.portfolio-content');
+        let   dot = document.querySelectorAll('.dot');
 
+
+        console.log(slide, btn, dot, slider,parrentDot);
+
+        let currentSlide = 0, // первый слайд
+            interval; //переменная для остановки счетчика
+
+        // СМЕНА КЛАССОВ СЛАЙДЕРА
+
+        // добавляем класс для следующего
+        const prevSlide = (element, index, removeClass) => { // element - элемент, index - индекс, addClass - удаляемый класс
+            element[index].classList.remove(removeClass);//добавить эфект
+        };
+        // удаляем класс у текущего
+        const nextSlide = (element, index, addClass) => { // element - элемент, index - индекс, addClass - добавляемый класс
+            element[index].classList.add(addClass); //добавить эфект
+        };
+
+        // авто воспроизведение слайдера
+        const autoPlaySlide = ()=>{
+            prevSlide( slide, currentSlide, 'portfolio-item-active');
+            prevSlide( dot, currentSlide, 'dot-active');
+            //  slide[currentSlide].classList.remove('portfolio-item-active');
+             currentSlide++;
+             if (currentSlide >= slide.length) {
+                 currentSlide = 0;
+             }
+             nextSlide( slide, currentSlide, 'portfolio-item-active');
+             nextSlide( dot, currentSlide, 'dot-active');
+
+            //  slide[currentSlide].classList.add('portfolio-item-active');
+
+        };
+        // запуск слайдера
+        const startSlide = ()=>{
+            let startSliderItem = (time = 3000) =>{ //значение по умолчанию 3 сек (3000)
+                interval = setInterval(autoPlaySlide, time);
+            };
+            startSliderItem(); //скорость переключения слайдов
+        };
+        // остановка слайдера
+        const stopSlide = ()=>{
+                clearInterval(interval);
+        };
+        
+      
+        // обработчик нажатия на кнопки и точки слайдера
+        slider.addEventListener('click',()=>{
+            event.preventDefault();
+            let target = event.target;
+            // условия вызова обработчика 
+            if (!target.matches('.portfolio-btn', '.dot')) { // если в таргет отсутствуют селекторы то ничего не делаем
+                // if (!target.matches('#arrow-right','#arrow-left','.dot')) { // если в таргет отсутствуют селекторы то ничего не делаем
+                return;            
+            }
+
+            prevSlide( slide, currentSlide, 'portfolio-item-active');
+            prevSlide( dot, currentSlide, 'dot-active');
+
+            if (target.matches('#arrow-right')) { // если в таргете есть id #arrow-ride
+                currentSlide++;
+            }else if (target.matches('#arrow-left')) {
+                currentSlide--;
+            } else if (target.matches('.dot')) {
+                dot.forEach((element, index)=>{
+                    if(element === target){
+                        currentSlide = index;
+                    }
+                });
+            }
+            // если кнопками дошелкали до последнего элемента то следующий элемент присваеваем 0й элемент массива
+            if (currentSlide >= slide.length ) {
+                currentSlide = 0;
+            }
+            // если кнопками дошелкали до первого элемента то следующий элемент присваеваем последний элемент массива
+            if (currentSlide < 0 ) {
+                currentSlide = slide.length -1;
+            }
+
+            nextSlide( slide, currentSlide, 'portfolio-item-active');
+            nextSlide( dot, currentSlide, 'dot-active');
+
+        });
+        // остановка слайдера при наведении мышкой на кнопки или точки
+        slider.addEventListener('mouseover',(event)=>{ // не нужно использовать mouseenter
+            let target = event.target;
+
+            if (target.matches('.portfolio-btn') || target.matches('.dot')) { // если мышь прибежала на эти элементы
+                stopSlide();
+            }
+            
+        });
+        // запуск слайдера если мышь убежала
+        slider.addEventListener('mouseout',(event) => { // не нужно использовать mouseleave
+            let target = event.target;
+
+            if (target.matches('.portfolio-btn') || target.matches('.dot')) { // если мышь убежала с этих элементов
+                startSlide();
+            }
+
+        });
+
+        // количество точек и добавление в слайдер
+        const enterDotScrean = () => {
+            let elementLi;
+
+                slide.forEach((element,i) => {
+                    if (i>0) {
+                        elementLi = document.createElement('li');
+                        elementLi.classList.add('dot'); 
+                        parrentDot.appendChild(elementLi); 
+                    } else{
+                        elementLi = document.createElement('li');
+                        elementLi.classList.add('dot'); 
+                        elementLi.classList.add('dot-active'); 
+                        parrentDot.appendChild(elementLi);
+                    }
+                    
+                    console.log(i);
+                });
+                dot = document.querySelectorAll('.dot');
+
+
+        };
+        enterDotScrean();
+
+        startSlide();
     };
     slider();
+    
 
 });
 
